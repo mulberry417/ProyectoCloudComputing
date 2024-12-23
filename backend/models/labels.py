f24848cacbf63d8bbcca7fdc3ea65a51268b69b2
Angely Mendez Cruz <130884896+verdejupiter@users.noreply.github.com>
2024-12-23 00:42:02 -0500

    Delete backend directory

--- backend/models/labels.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(r'C:\Users\Usuario\Documents\proyectoCloud\backend\models\yolov8n.pt')
-
-print("Etiquetas disponibles en el modelo preentrenado:")
-print(model.names)
